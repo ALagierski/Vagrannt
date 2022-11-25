a095dad2c75e4ff7a678d129bd02703ba16ea0e4
ALagierski <aleksanderlagierski@gmail.com>
2022-11-25 19:01:52 +0000

    Improve script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-sudo apt install bash-completion
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+sudo apt install bash-completion &>/dev/null
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash &>/dev/null
 
-cat << EOF >> ~/.bashrc
+
+grep "git-cimpletion" $HOME/.bashrc || cat << EOF >> $HOME/.bashrc
 if [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash
 fi
 EOF
 
-chmod +x ~/.git-completion.bash
-sudo wget https://raw.githubusercontent.com/dysosmus/ansible-completion/master/ansible-completion.bash -P /etc/bash_completion.d/
+chmod +x $HOME~/.git-completion.bash
+sudo wget https://raw.githubusercontent.com/dysosmus/ansible-completion/master/ansible-completion.bash -P /etc/bash_completion.d/ &>/dev/null
 
 for file in doc galaxy playbook pull vault
 do
-    sudo wget https://raw.githubusercontent.com/dysosmus/ansible-completion/master/ansible-$file-completion.bash -P /etc/bash_completion.d/
+    sudo wget https://raw.githubusercontent.com/dysosmus/ansible-completion/master/ansible-$file-completion.bash -P /etc/bash_completion.d/ &>/dev/null
 done
