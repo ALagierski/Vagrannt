d5eab013bbab7c88a329f46b833101d85da00b13
ALagierski <aleksanderlagierski@gmail.com>
2022-11-25 19:08:56 +0000

    Fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,13 +4,13 @@ sudo apt install bash-completion &>/dev/null
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash &>/dev/null
 
 
-grep "git-cimpletion" $HOME/.bashrc || cat << EOF >> $HOME/.bashrc
+grep "git-cimpletion" /home/panda/.bashrc || cat << EOF >> /home/panda/.bashrc
 if [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash
 fi
 EOF
 
-chmod +x $HOME~/.git-completion.bash
+chmod +x /home/panda/.git-completion.bash
 sudo wget https://raw.githubusercontent.com/dysosmus/ansible-completion/master/ansible-completion.bash -P /etc/bash_completion.d/ &>/dev/null
 
 for file in doc galaxy playbook pull vault
